159f0084c0ea27d963cfc0e0190eba715345570f
artemiokost <artemiokost@gmail.com>
2023-07-31 03:39:03 +0700

    x-client -- sass upd

--- src/app/components/containers/errors/NotFound.tsx
+++ src/app/components/containers/errors/NotFound.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const NotFound = ({ message }) => (
+const NotFound = ({ message }: { message: string }) => (
   <div className="column p-4 is-centered">
     <p className="has-text-centered">
       {message ? message : "Страница не найдена :("}
