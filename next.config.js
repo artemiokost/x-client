f22ba50f94cb82576b41616d3d29b3d210a44f83
artemiokost <artemiokost@gmail.com>
2023-08-02 21:42:36 +0700

    x-client -- spa tweak

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  async rewrites() {
+    return [
+      // Rewrite everything else to use `pages/index`
+      {
+        source: '/:path*',
+        destination: '/',
+      },
+    ]
+  },
+}
